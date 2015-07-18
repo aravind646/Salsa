318e401569f0dcb3e5a69fc582635bcd5868ce45
Vishvesh Shah <vmshah@scu.edu>
2015-07-18 16:07:34 -0700

    database connection

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
+var cps = require('cps-api');
+
 exports.index = function (req, res) {
+
+    var cpsConn = new cps.Connection(  'tcp://cloud-us-0.clusterpoint.com:9007',  'user',     'root',    'aaa','document','document/id', {account: 100884});
+    var id = 1,
+        name = "Username";
+    var insert_request = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(name, "Username")+'</document>');
+    cpsConn.sendRequest(insert_request, function(err, insert_response) {
+        if (err) return console.error(err);
+        console.log('New user registered: ' + insert_response.document.id);
+    });
     res.render('index');
 };
