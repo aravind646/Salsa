923b716ad4dd4e9784d4ec09fc851826da522e1d
Vishvesh Shah <vmshah@scu.edu>
2015-07-18 18:38:42 -0700

    changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
+
 var routes = require('./routes/');
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,36 @@
 var cps = require('cps-api');
+var watson = require('watson-developer-cloud');
+
+var personality_insights = watson.personality_insights({
+    username: '8d119473-86a5-4cc4-8da8-e483a88f0397',
+    password: 'gV6O9XgYRqXQ',
+    version: 'v2'
+});
+
 
 exports.index = function (req, res) {
 
     var cpsConn = new cps.Connection(  'tcp://cloud-us-0.clusterpoint.com:9007',  'user',     'root',    'aaa','document','document/id', {account: 100884});
 
     //Data
-    var FB = '{"id":"sd7944472d871252","name":"Kirill Mangutov","posts":["California is pretty cool","Jello"]}';
+    var FB = '{"id":"sd7944472d87wew1252","name":"Kirill Mangutov","posts":["California is pretty cool","Jello"]}';
     var json = JSON.parse(FB);
     var essay ='Vice President Johnson, Mr. Speaker, Mr. Chief Justice, President Eisenhower, Vice President Nixon, President Truman, Reverend Clergy, fellow citizens:We observe today not a victory of party but a celebration of freedom--symbolizing an end as well as a beginning--signifying renewal as well as change. For I have sworn before you and Almighty God the same solemn oath our forbears prescribed nearly a century and three-quarters ago. The world is very different now. For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life. And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe--the belief that the rights of man come not from the generosity of the state but from the hand of God.';
     var id = json.id;
     var name =json.name;
     var data = json.posts;
     var analysis = data+' '+essay;
+
+    personality_insights.profile({
+            text: 'Vice President Johnson, Mr. Speaker, Mr. Chief Justice, President Eisenhower, Vice President Nixon, President Truman, Reverend Clergy, fellow citizens:We observe today not a victory of party but a celebration of freedom--symbolizing an end as well as a beginning--signifying renewal as well as change. For I have sworn before you and Almighty God the same solemn oath our forbears prescribed nearly a century and three-quarters ago. The world is very different now. For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life. And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe--the belief that the rights of man come not from the generosity of the state but from the hand of God.',
+            language: 'en' },
+        function (err, response) {
+            if (err)
+                console.log('error:', err);
+            else
+                console.log(JSON.stringify(response, null, 2));
+        });
+
     var insert_request = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(name,"User")+cps.Term(data,"Data")+cps.Term(essay,"essay")+'</document>');
     cpsConn.sendRequest(insert_request, function(err, insert_response) {
         if (err) return console.error(err);
