65f5a066a225a1d18e527179fb1e0520b4bb605b
Vishvesh Shah <vmshah@scu.edu>
2015-07-18 21:46:35 -0700

    its working

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ exports.index = function (req, res) {
     var cpsConn = new cps.Connection(  'tcp://cloud-us-0.clusterpoint.com:9007',  'user',     'root',    'aaa','document','document/id', {account: 100884});
 
     //Data
-    var FB = '{"id":"sd7944472d87wew1252","name":"Kirill Mangutov","posts":["California is pretty cool","Jello"]}';
+    var FB = '{"id":"dsd7944472d87wew1252","name":"Kirill Mangutov","posts":["California is pretty cool","Jello"]}';
     var json = JSON.parse(FB);
     var essay ='Vice President Johnson, Mr. Speaker, Mr. Chief Justice, President Eisenhower, Vice President Nixon, President Truman, Reverend Clergy, fellow citizens:We observe today not a victory of party but a celebration of freedom--symbolizing an end as well as a beginning--signifying renewal as well as change. For I have sworn before you and Almighty God the same solemn oath our forbears prescribed nearly a century and three-quarters ago. The world is very different now. For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life. And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe--the belief that the rights of man come not from the generosity of the state but from the hand of God.';
     var id = json.id;
@@ -22,19 +22,21 @@ exports.index = function (req, res) {
     var analysis = data+' '+essay;
 
     personality_insights.profile({
-            text: 'Vice President Johnson, Mr. Speaker, Mr. Chief Justice, President Eisenhower, Vice President Nixon, President Truman, Reverend Clergy, fellow citizens:We observe today not a victory of party but a celebration of freedom--symbolizing an end as well as a beginning--signifying renewal as well as change. For I have sworn before you and Almighty God the same solemn oath our forbears prescribed nearly a century and three-quarters ago. The world is very different now. For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life. And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe--the belief that the rights of man come not from the generosity of the state but from the hand of God.',
+            text: analysis,
             language: 'en' },
         function (err, response) {
             if (err)
                 console.log('error:', err);
             else
-                console.log(JSON.stringify(response, null, 2));
-        });
+            var first_val = (response.tree.children[2].children[0].percentage);
+            var sec_val = (response.tree.children[1].children[0].percentage);
+            var thi_val = (response.tree.children[2].children[0].percentage);
+            var insert_request = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(name,"User")+cps.Term(data,"Data")+cps.Term(essay,"essay")+cps.Term(first_val,"Openness_parent")+cps.Term(sec_val,"Curiosity_parent")+cps.Term(thi_val,"Conservation_parent")+'</document>');
+            cpsConn.sendRequest(insert_request, function(err, insert_response) {
+                if (err) return console.error(err);
+                console.log('New user registered: ');});
 
-    var insert_request = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(name,"User")+cps.Term(data,"Data")+cps.Term(essay,"essay")+'</document>');
-    cpsConn.sendRequest(insert_request, function(err, insert_response) {
-        if (err) return console.error(err);
-        console.log('New user registered: ');
-    });
-    res.render('index');
+            res.render('index');
+        });
 };
+
